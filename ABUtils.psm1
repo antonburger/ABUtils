431380ffa99804d223e9a9a29d1577461bf0eed5
Anton Burger <burgeranton@gmail.com>
2019-09-09 02:09:07 +0100

    Autocomplete revision and file

--- ABUtils.psm1
+++ ABUtils.psm1
@@ -4,16 +4,79 @@ function Get-GitFileContent
     param
     (
         [Parameter()]
+        [ValidateNotNullOrEmpty()]
         [Alias('Repo')]
-        [IO.DirectoryInfo]
-        $RepoDirectory = './.git',
+        [string]
+        $RepoDirectory = '.',
+
         [Parameter(Mandatory, Position = 0)]
         [ValidateNotNullOrEmpty()]
+        [ArgumentCompleter({
+            param ($commandName, $paramName, $wordToComplete, $commandAst, $boundParameters)
+
+            $repo = if ($boundParameters.ContainsKey('RepoDirectory')) { $boundParameters.RepoDirectory } else { '.' }
+            $repo = Resolve-GitRepo $repo -ErrorAction SilentlyContinue
+            if ($null -eq $repo) { return @() }
+
+            $revisions = @(& git -C "$repo" for-each-ref --format='%(refname:short)') +
+                @(& git -C "$repo" for-each-ref --format='%(refname)') +
+                @(& git -C "$repo" rev-list --all --abbrev-commit)
+
+            $revisions | Where-Object { $_ -ilike "${wordToComplete}*" }
+        })]
         [string]
-        $Revision = 'master',
+        $Revision,
+
         [Parameter(Mandatory, Position = 1)]
         [ValidateNotNullOrEmpty()]
+        [ArgumentCompleter({
+            param ($commandName, $paramName, $wordToComplete, $commandAst, $boundParameters)
+
+            if (-not $boundParameters.ContainsKey('Revision')) { return @() }
+            $rev = $boundParameters.Revision
+            $repo = if ($boundParameters.ContainsKey('RepoDirectory')) { $boundParameters.RepoDirectory } else { '.' }
+            $repo = Resolve-GitRepo $repo -ErrorAction SilentlyContinue
+            if ($null -eq $repo) { return @() }
+
+            $files = & git -C "$repo" ls-tree -r --name-only "$rev"
+
+            if ($LASTEXITCODE -ne 0) { return @() }
+
+            $files | Where-Object { $_ -ilike "${wordToComplete}*" }
+        })]
         [string]
-        $Path
+        $FilePath
     )
+
+    $gitdir = Resolve-GitRepo $RepoDirectory
+
+    $result = & git -C "$gitdir" cat-file -p "${Revision}:${FilePath}" 2>&1
+
+    if ($LASTEXITCODE -ne 0) {
+        throw $result
+    }
+
+    $result
+}
+
+function Resolve-GitRepo
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [Alias('Repo')]
+        [string]
+        $RepoDirectory = '.'
+    )
+
+    $RepoDirectory = Resolve-Path -Path $RepoDirectory -ErrorAction Stop
+    $gitdir = & git -C "$RepoDirectory" rev-parse --absolute-git-dir 2>&1
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error "Not a git directory: $RepoDirectory"
+        return $null
+    }
+
+    Resolve-Path $gitdir
 }
