60c8f4244a9c9bdf268440d2f4aad054a5bb68dc
Anton Burger <burgeranton@gmail.com>
2019-09-10 23:41:47 +0100

    Refactor autocompletion logic to standalone functions

--- ABUtils.psd1
+++ ABUtils.psd1
@@ -67,7 +67,7 @@ Copyright = '(c) Anton Burger. All rights reserved.'
 # NestedModules = @()
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-FunctionsToExport = 'Get-GitFileContent','Resolve-GitRepo'
+FunctionsToExport = 'Get-GitFileContent','Resolve-GitRepo','Get-GitRevisions','Get-GitRevisionFiles'
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
 CmdletsToExport = @()
--- ABUtils.psm1
+++ ABUtils.psm1
@@ -17,14 +17,8 @@ function Get-GitFileContent
             param ($commandName, $paramName, $wordToComplete, $commandAst, $boundParameters)
 
             $repo = if ($boundParameters.ContainsKey('RepoDirectory')) { $boundParameters.RepoDirectory } else { '.' }
-            $repo = Resolve-GitRepo $repo -ErrorAction SilentlyContinue
-            if ($null -eq $repo) { return @() }
 
-            $revisions = @(& git -C "$repo" for-each-ref --format='%(refname:short)') +
-                @(& git -C "$repo" for-each-ref --format='%(refname)') +
-                @(& git -C "$repo" rev-list --all --abbrev-commit)
-
-            $revisions | Where-Object { $_ -ilike "${wordToComplete}*" }
+            Get-GitRevisions -RepoDirectory $repo -ErrorAction SilentlyContinue | Where-Object { $_ -ilike "${wordToComplete}*" }
         })]
         [string]
         $Revision,
@@ -34,17 +28,11 @@ function Get-GitFileContent
         [ArgumentCompleter({
             param ($commandName, $paramName, $wordToComplete, $commandAst, $boundParameters)
 
-            if (-not $boundParameters.ContainsKey('Revision')) { return @() }
             $rev = $boundParameters.Revision
+            if ($null -eq $rev) { return @() }
             $repo = if ($boundParameters.ContainsKey('RepoDirectory')) { $boundParameters.RepoDirectory } else { '.' }
-            $repo = Resolve-GitRepo $repo -ErrorAction SilentlyContinue
-            if ($null -eq $repo) { return @() }
 
-            $files = & git -C "$repo" ls-tree -r --name-only "$rev"
-
-            if ($LASTEXITCODE -ne 0) { return @() }
-
-            $files | Where-Object { $_ -ilike "${wordToComplete}*" }
+            Get-GitRevisionFiles -RepoDirectory $repo -Revision $rev -ErrorAction SilentlyContinue | Where-Object { $_ -ilike "${wordToComplete}*" }
         })]
         [string]
         $FilePath
@@ -81,4 +69,60 @@ function Resolve-GitRepo
     }
 
     Resolve-Path $gitdir
+}
+
+function Get-GitRevisions
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [Alias('Repo')]
+        [string]
+        $RepoDirectory = '.'
+    )
+
+    $repo = Resolve-GitRepo -RepoDirectory $RepoDirectory
+    if ($null -eq $repo)
+    {
+        return $null
+    }
+
+    @(& git -C "$repo" for-each-ref --format='%(refname:short)') +
+        @(& git -C "$repo" for-each-ref --format='%(refname)') +
+        @(& git -C "$repo" rev-list --all --abbrev-commit)
+}
+
+function Get-GitRevisionFiles
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [Alias('Repo')]
+        [string]
+        $RepoDirectory = '.',
+
+        [Parameter(Mandatory, Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Revision
+    )
+
+    $repo = Resolve-GitRepo -RepoDirectory $RepoDirectory
+    if ($null -eq $repo)
+    {
+        return $null
+    }
+
+    $files = & git -C "$repo" ls-tree -r --name-only "$Revision"
+    if ($LASTEXITCODE -ne 0)
+    {
+        Write-Error $files
+        return $null
+    }
+
+    $files
 }
